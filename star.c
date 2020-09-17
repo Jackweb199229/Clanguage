cb705f90cf7771e1c22e4ed643e98357e822c10f
MD Jafar <69416964+Jackweb199229@users.noreply.github.com>
2020-09-17 23:54:03 +0530

    Add files via upload

---
+++ star.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+ {
+ int i,j;
+ for(i=1;i<=6;i++)
+ {
+     for(j=1;j<=11;j++)
+     {
+         if(j>=7-i && j<=5+i)
+         printf ("*");
+         else
+         printf (" ");
+         
+     }
+     printf ("\n");
+     
+ }
+ 
+ 
+ 
+    return 0;
+}
